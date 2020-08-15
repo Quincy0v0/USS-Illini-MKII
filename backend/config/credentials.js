e1caf7594d5e1c8c51a225427d6c364c12ed5940
Chen Pan <30424046+Quincy0v0@users.noreply.github.com>
2020-08-15 04:49:29 -0700

    change file structure

--- backend/config/credentials.js
+++
@@ -1,7 +0,0 @@
-/**
- * Database access credentials
- */
-module.exports = {
-    token : "Queeny",
-    mongo_connection : "mongodb+srv://chuangk2:Tea890122@caldeadb-buuxb.mongodb.net/test?retryWrites=true"
-};
